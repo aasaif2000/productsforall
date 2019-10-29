506d8eccb6090a5059157318ef0a5c8871a1ff2a
aasaif2000 <aasaif2000@hotmail.com>
2019-10-28 19:13:53 -0500

    10282019

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
 
+import { Todos } from "./productsall";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'productsforall';
+  title = 'Angular Project of Products for All';
+
+todovalue: string;
+list:Todos[];
+
+ngOnInit() {
+this.list = [];
+this.todovalue = "";
+
+}
+
+addItem(){
+
+  if(this.todovalue !== ""){
+    const newItem: Todos = {
+      id: Date.now(),
+      value: this.todovalue,
+      isDone: false
+    };
+    this.list.push(newItem);
+  }
+  this.todovalue= "";
+}
+
+deleteitem(id: number){
+this.list = this.list.filter(item => item.id !== id);
+
+}
 }
